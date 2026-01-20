f0d3b8c6745cc484931b0b0399bcb9c6b9cb56a4
Tyler <tyler.s9@proton.me>
2026-01-20 01:51:45 -0500

    Finished the challenge. Now to check my answers

--- AreaOfATriangleChallenge/src/main/java/com/udemy/myCode/CuboidExample.java
+++ AreaOfATriangleChallenge/src/main/java/com/udemy/myCode/CuboidExample.java
@@ -2,17 +2,45 @@ package com.udemy.mycode;
 import java.util.Scanner;
 
 /* 
-    ** Prompt: Determine the amount of paint required to paint the walls of a given cuboid.
-    - Height, length, and breadth must be given.
+    *** Prompt: Determine the [ total area ] of a cuboid based on its l, w, and h.
+    * Height, length, and breadth must be given.
+    * Must find the front/back area, the right/left area, and the bottom/top area.
+    * Volume is equal to "len * breadth * height."
+    * Total area is the sum of the x, y, and z areas (right/left, bottom/top, & front/back) + the vol of the cuboid.
+    * Total area is the total of all 6 sides.
 */
 
 public class CuboidExample {
-    Scanner sc = new Scanner(System.in);
 
     public static void Hexahedron(){
-        //
+        Scanner sc = new Scanner(System.in);
+        int frontBack, topBottom, rightLeft, height, length, breadth;
+        long volume, totalArea;
+
+        System.out.println("Hi! What is the height of the hexahedron?");
+        height = sc.nextInt();
+
+        System.out.println("Now, What is the length of the hexahedron?");
+        length = sc.nextInt();
+
+        System.out.println("Lastly, What is the breadth of the hexahedron?");
+        breadth = sc.nextInt();
+        System.out.println(" ");
+
+        frontBack = length * height;
+        rightLeft = breadth * height;
+        topBottom = length * breadth;
+
+        volume = length * breadth * height;
+
+        totalArea = volume + (frontBack + rightLeft + topBottom);
+
+        System.out.println("######## ###### ###### ######");
+        System.out.println(" ");
+        System.out.println("The TOTAL AREA of the cuboid is: -> -> " + totalArea);
+
     }
     public static void main(String[] args) {
-        //
+        Hexahedron();
     }
 }

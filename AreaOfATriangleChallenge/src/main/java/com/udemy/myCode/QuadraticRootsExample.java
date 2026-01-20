7283d6d6c5adfb3cd97dfb196d137c45bead3ee5
Tyler <tyler.s9@proton.me>
2026-01-20 00:25:00 -0500

    Added basic code layout for QuadraticRootsExample.java

--- AreaOfATriangleChallenge/src/main/java/com/udemy/myCode/QuadraticRootsExample.java
+++ AreaOfATriangleChallenge/src/main/java/com/udemy/myCode/QuadraticRootsExample.java
@@ -1,9 +1,15 @@
 package main.java.com.udemy.myCode;
+import java.util.Scanner;
 
 /* 
 * This program is for finding the roots of a quadratic equation
 */
 
 public class QuadraticRootsExample {
-    //
+    /* We should use these vars:
+    - int a, b, c
+    - double r1, r2
+    */
+
+
 }

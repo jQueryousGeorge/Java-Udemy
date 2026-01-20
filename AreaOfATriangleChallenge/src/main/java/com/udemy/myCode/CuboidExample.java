77cf56f18b79c58535e7e6a28d1603b806199ede
Tyler <tyler.s9@proton.me>
2026-01-20 02:11:36 -0500

    Correct answer reached for the cuboid example challenge

--- AreaOfATriangleChallenge/src/main/java/com/udemy/myCode/CuboidExample.java
+++ AreaOfATriangleChallenge/src/main/java/com/udemy/myCode/CuboidExample.java
@@ -6,13 +6,13 @@ import java.util.Scanner;
     * Height, length, and breadth must be given.
     * Must find the front/back area, the right/left area, and the bottom/top area.
     * Volume is equal to "len * breadth * height."
-    * Total area is the sum of the x, y, and z areas (right/left, bottom/top, & front/back) + the vol of the cuboid.
     * Total area is the total of all 6 sides.
+    * INITIALLY, I though this was correct for total area: "Total area is the sum of the x, y, and z areas (right/left, bottom/top, & front/back) + the vol of the cuboid."
 */
 
 public class CuboidExample {
 
-    public static void Hexahedron(){
+    /* public static void Hexahedron(){
         Scanner sc = new Scanner(System.in);
         int frontBack, topBottom, rightLeft, height, length, breadth;
         long volume, totalArea;
@@ -39,8 +39,34 @@ public class CuboidExample {
         System.out.println(" ");
         System.out.println("The TOTAL AREA of the cuboid is: -> -> " + totalArea);
 
+    } */
+
+    public static void CorrectAnswer() {
+        Scanner sc = new Scanner(System.in);
+        int length, width, height;
+        int totalArea, volume;
+
+        System.out.println("Hi! What is the height of the hexahedron?");
+        height = sc.nextInt();
+
+        System.out.println("Now, What is the length of the hexahedron?");
+        length = sc.nextInt();
+
+        System.out.println("Lastly, What is the breadth/width of the hexahedron?");
+        width = sc.nextInt();
+        System.out.println(" ");
+
+        totalArea = 2 * (length * width + length * height + width * height);
+        volume = length * width * height;
+
+        System.out.println("######## ###### ###### ######");
+        System.out.println(" ");
+        System.out.println("The TOTAL AREA of the cuboid is: -> -> " + totalArea);
+        System.out.println(" ");
+        System.out.println("The VOLUME of the cuboid is: -> -> " + volume);
     }
+    
     public static void main(String[] args) {
-        Hexahedron();
+        CorrectAnswer();
     }
 }
